5c903ceb97a3fe368dce218cfbf2bb5feece547a
Ricky <rzh4321@gmail.com>
2023-05-22 18:40:46 -0400

    added event listener to each cell, clicking marks the cell

--- script.js
+++ script.js
@@ -5,22 +5,31 @@ const board = (() => {
         [null, null, null]
       ];
     let turn = 'X';
-    const play = (r, c) => {
-        if (board_array[r][c]) {
+    const play = (r, c, elem) => {
+        if (!board_array[r][c]) {
             board_array[r][c] = turn;
+            elem.textContent = turn;
             turn = (turn == 'X')? 'O' : 'X';
-            return true;
         }
-        return false;
+        else {
+            alert('Position already taken');
+        }
     }
     return {
-
+        play
     };
 })();
 
 let container = document.querySelector('.board');
-for (let i = 0; i < 9; ++i) {
-    let cell = document.createElement('div');
-    cell.classList.add('cell', 'valid');
-    container.append(cell);
+for (let r = 0; r < 3; ++r) {
+    for (let c = 0; c < 3; ++c) {
+        let cell = document.createElement('div');
+        cell.classList.add('cell', 'valid');
+        // cell.setAttribute('data-row', r);
+        // cell.setAttribute('data-col', c);
+        cell.addEventListener('click', function(e) {
+            board.play(r, c, cell);
+        });
+        container.append(cell);
+    }
 }
